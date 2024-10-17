23a6dd462b5b7ffe224098412147d44c2b9d9ba4
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-10-18 00:30:56 +0530

    created a song card

---
+++ src/components/Card.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Card = () => {
+    const data = [
+        { song: "Mocking Bird", description: "This is a song by Eminem, one of my all-time favorites." },
+        { song: "Maazak", description: "This is a song by Anuv Jain" }
+    ];
+    const handleClickDownload = () => { alert("Click"); };
+    return (
+        <div className="w-full h-screen bg-zinc-300 flex flex-col gap-4 justify-center items-center">
+            {data.map((ele, index) => (
+                <div key={index} className="song px-3 py-2 w-90 bg-zinc-100 rounded">
+                    <h3 className="font-semibold text-xl  w-64">{ele.song}</h3> {/* Set width */}
+                    <p className="text-xs mt-2  w-64">{ele.description}</p> {/* Set width */}
+                    <button onClick={handleClickDownload} className="px-2 py-1 bg-blue-400 text-sm font-semibold text-zinc-100 rounded-md mt-3">
+                        Download Now
+                    </button>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default Card;

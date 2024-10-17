8861941abf7f1c0f6a486ac6d50648704e385df4
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-10-18 00:34:06 +0530

    learnt useState advanced

---
+++ src/components/UseStateAdvanced.jsx
@@ -0,0 +1,15 @@
+import React, { useState } from "react";
+
+const UseStateAdvanced = () => {
+    const [val, setval] = useState(12);
+    return (
+        <div className="p-4">
+            <h1>{val}</h1>
+            <button onClick={()=>setval((prev)=>prev+1)} className="px-3 py-1 bg-blue-400 rounded-full">Change karo</button>
+        </div>
+        
+
+    );
+};
+
+export default UseStateAdvanced;
